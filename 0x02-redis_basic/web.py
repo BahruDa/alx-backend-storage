0dc28525f0a0a4f53c6249165c798b143d81880e
BahruDa <bahrudari68@gmail.com>
2023-08-16 18:21:05 +0300

    hot fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python3
-"""Module tracks how many times a particular URL was accessed"""
-import redis
 import requests
-from typing import Callable
-from functools import wraps
+import redis
+import functools
 
+# Connect to the Redis server
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def count(method: Callable):
-    '''track how many times a particular URL was accessed'''
-    r = redis.Redis()
+# Decorator to implement caching and tracking
+def cached_and_tracked(expiration=10):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(url):
+            cache_key = f"cache:{url}"
+            count_key = f"count:{url}"
+            
+            # Check if the result is already cached
+            cached_result = redis_client.get(cache_key)
+            if cached_result:
+                # Increment the access count
+                redis_client.incr(count_key)
+                return cached_result.decode('utf-8')
+            
+            # Fetch the page content
+            response = requests.get(url)
+            if response.status_code == 200:
+                content = response.text
+                # Cache the result with an expiration time
+                redis_client.setex(cache_key, expiration, content)
+                # Increment the access count
+                redis_client.incr(count_key)
+                return content
+            else:
+                return f"Error: Unable to fetch page from {url}"
+        
+        return wrapper
+    return decorator
 
-    @wraps(method)
-    def wrapper(url):
-        '''wrap the decorated function and return the wrapper'''
-        r.incr(f"count:{url}")
-        exp_count = r.get(f"cached:{url}")
-        if exp_count:
-            return exp_count.decode('utf-8')
-        html = method(url)
-        r.setex(f"cached:{url}", 10, html)
-        return html
-    return wrapper
-
-
-@count
+# Apply the decorator to the get_page function
+@cached_and_tracked(expiration=10)
 def get_page(url: str) -> str:
-    '''uses the requests module to obtain the HTML content'''
     return requests.get(url).text
+
+# Test the get_page function
+if __name__ == '__main__':
+    slow_url = "http://slowwly.robertomurray.co.uk/delay/10000/url/http://example.com"
+    print(get_page(slow_url))
